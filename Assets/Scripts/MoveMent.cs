bfab385a378d002be749737a84977761b6753262
HusseinOpp <54945659+HusseinOpp@users.noreply.github.com>
2025-07-31 02:16:41 +0300

    PlayerMovement

---
+++ Assets/Scripts/MoveMent.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class MoveMent : MonoBehaviour
+{
+    public float speed = 5f;
+
+
+    void Update()
+    {
+        float mvoex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(mvoex, 0, movey) * speed;
+        transform.Translate(movement, Space.World);
+    }
+}
