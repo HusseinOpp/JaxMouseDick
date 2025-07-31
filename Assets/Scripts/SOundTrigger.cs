def1a2ea803a9d97dd3514b12a09a3f0eda70c35
HusseinOpp <54945659+HusseinOpp@users.noreply.github.com>
2025-07-31 03:22:43 +0300

    dead audio

--- Assets/Scripts/SOundTrigger.cs
+++ Assets/Scripts/SOundTrigger.cs
@@ -1,11 +1,13 @@
+using System;
 using UnityEngine;
 
 public class SOundTrigger : MonoBehaviour
 {
     public AudioClip SOund;
+    public  String Tag;
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag(Tag))
         {
             AudioSource.PlayClipAtPoint(SOund, transform.position);
             Debug.Log("Enter");
